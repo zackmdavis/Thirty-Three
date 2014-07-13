3c216367a1a34b165b431031665e9fe9332a7938
Zack M. Davis <code@zackmdavis.net>
2014-07-13 13:26:00 -0700

    arena-size as argument to clean-n-arena

--- src/Thirty_Three/core.clj
+++ src/Thirty_Three/core.clj
@@ -1,14 +1,15 @@
 (ns Thirty-Three.core)
 
-(def arena-size 4)
-
-(defmacro clean-n-arena [n]
+(defmacro clean-n-arena [n arena-size]
+  "Create an n-dimensional vector-of-vectors-of-&c. with side length
+   arena-size"
   (if (= n 0)
     :_
-    `(vec (for [_# (range arena-size)]
-            (clean-n-arena ~(dec n))))))
+    `(vec (for [_# (range ~arena-size)]
+            (clean-n-arena ~(dec n) ~arena-size)))))
 
-(def one-arena (clean-n-arena 1))
-(def two-arena (clean-n-arena 2))
-(def the-arena (clean-n-arena 3))
+(def one-arena (clean-n-arena 1 4))
+(def two-arena (clean-n-arena 2 4))
+(def the-arena (clean-n-arena 3 4)) 
+
 
