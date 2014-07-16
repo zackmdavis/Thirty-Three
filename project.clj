1a893422d3e4e9ef23d095f7026d01dd67d4f9b3
Zack M. Davis <code@zackmdavis.net>
2014-07-15 22:43:48 -0700

    use test-refresh plugin, clarify status of math.combinatorics dep

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/math.combinatorics "0.0.7"]
-                 [jline "0.9.94"]
-                 [org.clojure/clojurescript "0.0-2156"]
-                 [om "0.5.0"]]
-  :plugins [[lein-cljsbuild "1.0.2"]]
+                ;[org.clojure/math.combinatorics "0.0.7"] <= needed fn inlined in
+                 [jline "0.9.94"]                        ; combinatorics-library
+                 [om "0.5.0"]                            ; because reasons
+                 [org.clojure/clojurescript "0.0-2156"]]
+  :plugins [[lein-cljsbuild "1.0.2"]
+            [com.jakemccrary/lein-test-refresh "0.5.0"]]
   :cljsbuild { 
     :builds [{:id "thirty-three"
               :source-paths ["src"]
