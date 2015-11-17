a4bd5136a087816ca76bc514d32147fbcb506b51
Zack M. Davis <code@zackmdavis.net>
2015-11-16 22:12:59 -0800

    :earth_americas: :arrow_up: the world changes, bump dependencies
    In particular, in the intervening year since this project has seen major
    activity, the fb.me CDN stopped serving React 0.9 entirely. After
    swapping out for a recent React, bumping our dependencies, and
    rebuilding the world after a `lein clean`, everything still works.

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "https://github.com/zackmdavis/Thirty-Three"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2156"]
-                ;[org.clojure/math.combinatorics "0.0.7"] <= needed fn inlined 
-                 [om "0.5.0"]                        ; in combinatorics-library
-                 [jayq "2.5.2"]]                     ; because reasons
-  :plugins [[lein-cljsbuild "1.0.2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                ;[org.clojure/math.combinatorics "0.0.7"] <= needed fn inlined
+                 [org.omcljs/om "0.9.0"]             ; in combinatorics-library
+                 [jayq "2.5.4"]]                     ; because reasons
+  :plugins [[lein-cljsbuild "1.1.1"]
             [com.jakemccrary/lein-test-refresh "0.5.0"]]
-  :cljsbuild { 
+  :cljsbuild {
     :builds [{:id "thirty-three"
               :source-paths ["src"]
               :compiler {
